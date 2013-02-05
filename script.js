dbf26afaf1ff97013cd237ecd21179d867d01ee7
Sunil Pai <threepointone@gmail.com>
2013-02-05 16:45:26 +0530

    copy edits

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var gol = require('./index.js')({
+var grid = require('./index.js')({
     width: 100,
     height: 20
 });
@@ -7,10 +7,12 @@ var gol = require('./index.js')({
 for(var i = 0; i < 200; i++) {
     var x = Math.ceil(Math.random() * 100);
     var y = Math.ceil(Math.random() * 20);
-    gol.at(x, y, true);
+    grid.at(x, y, true);
 }
 
 setInterval(function() {
-    gol.step();
-    console.log(gol.str());
+    grid.step();
+    console.log(grid.str());
 }, 100);
+
+// grid.at(x,y) will get you life/death at every cell. 
